47786b4e53c45e374ee071797e87030a93ab52d3
Lassi <timoskainen.lassi@gmail.com>
2023-05-20 15:01:22 +0300

    Added contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Contacts.module.scss';
+
 const ContactsPage = () => {
   return (
-    <div>
-      <h1>Contacts</h1>
+    <div className={styles.contacts}>
+      <div className={styles.contacts__container}>
+        <h1 className={styles.contacts__title}>TTYMI</h1>
+        <Image className={styles.contacts__image} src={require('../../public/logo.png')} alt='TTYMI' />
+        <p className={styles.contacts__social__text}>ttymi@lists.tuni.fi</p>
+        <Link className={styles.contacts__social} href='https://www.instagram.com/ttymi_ry/' target='_blank'>
+          <Image src={require('../../public/insta.svg')} alt='instagram' />
+          <p className={styles.contacts__social__text}>Instagram</p>
+        </Link>
+        <Link className={styles.contacts__social} href='https://www.facebook.com/Teekkarivisa3/' target='_blank'>
+          <Image src={require('../../public/face.svg')} alt='facebook' />
+          <p className={styles.contacts__social__text}>Facebook</p>
+        </Link>
+      </div>
     </div>
   );
 };
