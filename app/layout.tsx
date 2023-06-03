9448cedc6d2b010015a67ad552a825cb8b58c320
Lassi <timoskainen.lassi@gmail.com>
2023-06-03 13:34:25 +0300

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../styles/global.scss';
@@ -8,6 +9,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html>
       <link rel='shortcut icon' href={favicon.src} type='image/x-icon' />
       <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Analytics />
         <Navbar />
         <main style={{ flex: 1, marginTop: '3rem' }}>{children}</main>
         <Footer />
