429fc8683aa7a2971f8a204ac2de65e37509b343
Lassi <timoskainen.lassi@gmail.com>
2026-01-18 20:47:58 +0200

    Enhance event display styles for better readability and responsiveness

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -33,21 +33,27 @@ const EventPage = async ({ params }: any) => {
           </div>
         </div>
         <div className='m-[5rem_5rem_3rem_1rem] max-[1300px]:m-[2rem_1rem]'>
-          <h1 className='text-[3.5rem] font-semibold p-0 m-0 max-[768px]:text-[2.5rem]'>{event?.title}</h1>
-          <div className='flex flex-row items-center m-0 p-0 my-8'>
-            <Calendar1 className='w-8 h-8 md:w-6 md:h-6 text-ttymi-green' />
-            <p className='text-[1.75rem] font-semibold m-[0_1.5rem_0_0.5rem]'>{date.toLocaleDateString('fi-FI', {})}</p>
-            <Clock className='w-8 h-8 md:w-6 md:h-6 text-ttymi-green' />
-            <p className='text-[1.75rem] font-semibold m-[0_1.5rem_0_0.5rem]'>
-              {date.toLocaleTimeString('fi-FI', {
-                hour12: false,
-                hour: 'numeric',
-                minute: 'numeric',
-                timeZone: 'UTC',
-              })}
-            </p>
+          <h1 className='text-3xl md:text-[3.5rem] font-semibold p-0 m-0'>{event?.title}</h1>
+          <div className='flex flex-row items-center m-0 p-0 my-8 flex-wrap gap-y-4'>
+            <div className='flex items-center'>
+              <Calendar1 className='w-6 h-6 text-ttymi-green' />
+              <p className='text-lg md:text-[1.75rem] font-semibold m-[0_1.5rem_0_0.5rem]'>
+                {date.toLocaleDateString('fi-FI', {})}
+              </p>
+            </div>
+            <div className='flex items-center'>
+              <Clock className='w-6 h-6 text-ttymi-green' />
+              <p className='text-lg md:text-[1.75rem] font-semibold m-[0_1.5rem_0_0.5rem]'>
+                {date.toLocaleTimeString('fi-FI', {
+                  hour12: false,
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  timeZone: 'UTC',
+                })}
+              </p>
+            </div>
           </div>
-          <p className='text-2xl font-medium leading-[1.45] whitespace-pre-wrap max-[768px]:text-[1.25rem]'>
+          <p className='text-base md:text-2xl font-medium leading-[1.45] whitespace-pre-wrap break-words'>
             {event?.content}
           </p>
         </div>
